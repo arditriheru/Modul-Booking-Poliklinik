94e1b1791ee6f75dcf9e1053a2fe24bc8ed19ff1
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-05-05 16:01:18 +0700

    Create header.php

---
+++ views/header.php
@@ -0,0 +1,16 @@
+<?php error_reporting(0); ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Dashboard | Booking</title>
+    <!-- Bootstrap core CSS -->
+    <link href="../css/bootstrap.css" rel="stylesheet">
+    <!-- Add custom CSS here -->
+    <link href="../css/sb-admin.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/sweetalert.css">
+  </head>
