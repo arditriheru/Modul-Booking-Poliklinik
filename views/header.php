175b53c6897c50485847dab2f10cedd9c1ba0d60
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-05-11 10:45:49 +0700

    Update header.php

--- views/header.php
+++ views/header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Dashboard | Booking</title>
+    <title>SIMETRIS | Booking</title>
     <!-- Bootstrap core CSS -->
     <link href="../css/bootstrap.css" rel="stylesheet">
     <!-- Add custom CSS here -->
