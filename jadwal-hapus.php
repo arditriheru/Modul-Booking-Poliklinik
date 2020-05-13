2af640d07d3ecca36a1a04a72ed453c2b5e67b45
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-05-13 09:54:03 +0700

    Update jadwal-hapus.php

--- jadwal-hapus.php
+++ jadwal-hapus.php
@@ -1,11 +1,14 @@
 <?php include "readme.php";?>
-<?php 
-include '../koneksi.php';
-$id_jadwal = $_GET['id_jadwal'];
-$hapus=mysqli_query($koneksi,"DELETE FROM jadwal WHERE id_jadwal='$id_jadwal'");
-if($hapus){
-	echo "<script>alert('Berhasil Dihapus!!!');document.location='jadwal-dokter'</script>";
-    }else{
-    echo "<script>alert('Gagal Hapus!!!');document.location='jadwal-edit?id_jadwal=$id_jadwal'</script>";
-    }
+<?php
+	session_start();
+	include '../koneksi.php';
+		date_default_timezone_set("Asia/Jakarta");
+		$_SESSION['tanggal'] = date('Y-m-d');
+		$id_jadwal = $_GET['id_jadwal'];
+		$hapus=mysqli_query($koneksi,"DELETE FROM jadwal WHERE id_jadwal='$id_jadwal'");
+		if($hapus){
+			echo "<script>alert('Berhasil Dihapus!!!');document.location='jadwal-dokter'</script>";
+		    }else{
+		    echo "<script>alert('Gagal Hapus!!!');document.location='jadwal-edit?id_jadwal=$id_jadwal'</script>";
+		    }
 ?>
