8e1fcc18a77d555470b9dc375fc48ddabda3ce60
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-05-13 09:53:14 +0700

    Update jadwal-dokter.php

--- jadwal-dokter.php
+++ jadwal-dokter.php
@@ -19,7 +19,9 @@
         <div class="row">
           <div class="col-lg-12">
             <div class="row">
-              <div class="col-xs-12"><h4 class="bluetext"><b>Dokter Spesialis Anak</b></h4>
+              <div class="col-xs-12">
+                <div align="right">Diperbarui <?php echo format_update($update);?></div>
+                <h4 class="bluetext"><b>Dokter Spesialis Anak</b></h4>
             <table class="table table-bordered table-hover table-striped tablesorter">
                 <thead>
                     <tr>
