c4d04efc0390d0d8248de01a1c2408311489ee3e
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-06-09 15:31:54 +0700

    Add files via upload

--- booking-belum-datang-proses.php
+++ booking-belum-datang-proses.php
@@ -3,5 +3,5 @@
 	include '../koneksi.php';
 		$id_booking = $_GET['id_booking'];
 		mysqli_query($koneksi,"update booking set status = '2' where id_booking='$id_booking'");
-		header("location:dashboard");
+		header("location:booking-detail?id_booking=$id_booking");
 ?>
---
+++ tumbang-datang-proses.php
@@ -0,0 +1,7 @@
+<?php include "readme.php";?>
+<?php 
+	include '../koneksi.php';
+		$id_tumbang = $_GET['id_tumbang'];
+		mysqli_query($koneksi,"UPDATE tumbang SET status = '1' WHERE id_tumbang='$id_tumbang'");
+		header("location:dashboard");
+?>
