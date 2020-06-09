cebb2be866930fa84dc87a3720860f77417d87e7
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-06-09 15:47:33 +0700

    Delete auto-delete.php

--- auto-delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-include '../koneksi.php';
-$lama = 7;
-$query = "DELETE FROM booking
-          WHERE DATEDIFF(CURDATE(), booking_tanggal) > $lama";
-$hasil = mysqli_query($koneksi,$query);
-?>
